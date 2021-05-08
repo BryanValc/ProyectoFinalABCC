8716603416e1f7fc710af9ee44d2f4bbcb9c6285
Bryan Valdez <bryan501th@gmail.com>
2021-05-08 15:33:47 -0500

    Se hizo el DAO de Comprador y se hicieron priebas en el main

---
+++ src/controlador/CompradorDAO.java
@@ -0,0 +1,124 @@
+package controlador;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import conexionBD.ConexionBD;
+import modelo.Comprador;
+
+public class CompradorDAO {
+	
+	ConexionBD conexion;
+	
+	public CompradorDAO() {
+		conexion = new ConexionBD();
+		//this.conexion = conexion;
+	}
+	
+	public boolean insertarRegistro(Comprador c) {
+		boolean resultado = false;
+		
+		String sql="INSERT INTO Comprador VALUES("
+				+ c.getCompradorId()+","
+				+ "'"+c.getNombre()+"',"
+				+ "'"+c.getWallet()+"',"
+				+ "'"+c.getDireccion()+"',"
+				+ "'"+c.getCiudad()+"',"
+				+ "'"+c.getEstado()+"',"
+				+ "'"+c.getTelefono()+"',"
+				+ "'"+c.getEmail()+"'"
+				+ ")";
+		resultado = conexion.ejecutarInstruccion(sql);
+		
+		return resultado;
+	}
+	
+	public boolean eliminarRegistro(int compradorId) {
+		boolean resultado = false;
+		
+		String sql="DELETE FROM Comprador WHERE compradorId = "+compradorId;
+		resultado = conexion.ejecutarInstruccion(sql);
+		
+		return resultado;
+	}
+	
+	public boolean modificarRegistro(Comprador c, boolean flags[]) {
+		boolean resultado = false;
+		boolean primero=true;
+		
+		String sql = "UPDATE Comprador SET ";
+		
+		if (flags[0]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("nombre='"+c.getNombre()+"'");
+		}
+		if (flags[1]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("Wallet='"+c.getWallet()+"'");
+		}
+		if (flags[2]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("direccion='"+c.getDireccion()+"'");
+		}
+		if (flags[3]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("ciudad='"+c.getCiudad()+"'");
+		}
+		if (flags[4]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("estado='"+c.getEstado()+"'");
+		}
+		if (flags[5]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("telefono='"+c.getTelefono()+"'");
+		}
+		if (flags[6]) {
+			if (!primero) {sql+=", ";
+			}else {primero = false;}
+			sql+=("email='"+c.getEmail()+"'");
+		}
+		
+		sql+=(" WHERE compradorId = "+c.getCompradorId());
+		System.out.println(sql);
+		resultado = conexion.ejecutarInstruccion(sql);
+		
+		return resultado;
+	}
+
+	public ArrayList<Comprador> buscarCompradores(String filtro){
+		ArrayList<Comprador> listaCompradores = new ArrayList<Comprador>();
+		
+		ResultSet rs;
+		
+		rs = conexion.ejecutarConsulta(filtro);
+		//SELECT * FROM Alumnos WHERE prod_name = 'Microsoft 10-20 Keyboard' AND prod_price < 30;
+		try {
+			if (rs.next()) {
+				do {
+					listaCompradores.add(new Comprador(
+							rs.getInt(1),
+							rs.getString(2),
+							rs.getString(3),
+							rs.getString(4),
+							rs.getString(5),
+							rs.getString(6),
+							rs.getString(7),
+							rs.getString(8)));
+				} while (rs.next());			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return listaCompradores;
+	}
+	
+	
+}
--- src/vista/VentanaInicio.java
+++ src/vista/VentanaInicio.java
@@ -1,11 +1,24 @@
 package vista;
 
+import java.util.ArrayList;
+
+import controlador.CompradorDAO;
+import modelo.Comprador;
+
 public class VentanaInicio {
 
 	public static void main(String[] args) {
 		
-		
-		
+		CompradorDAO compradorDAO = new CompradorDAO();
+		Comprador comprador = new Comprador(1212, "Bryan", "1x1E1SA123123", "Arq Damaso", "Jerez", "Zacatecas", "494-118-9287", "bryan.valdez117@outlook.es");
+		//compradorDAO.insertarRegistro(comprador);
+		//compradorDAO.eliminarRegistro(1212);
+		Comprador comprador2 = new Comprador(1212, "Juan", "1x1E1SA123123", "Arq Damaso", "Zacatecas", "Zacatecas", "494-118-9287", "bryan.valdez117@outlook.es");
+		boolean flags[]=new boolean[7];
+		flags[0]=true;
+		compradorDAO.modificarRegistro(comprador2, flags);
+		ArrayList<Comprador> compradores = compradorDAO.buscarCompradores("SELECT * FROM Comprador WHERE nombre = 'Bryan'");
+		System.out.println(compradores);
 	}
 
 }
