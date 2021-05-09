47020c72e39921ee452509a3652c03c4e08dc7f6
Bryan Valdez <bryan501th@gmail.com>
2021-05-09 15:17:56 -0500

    En actionPerformed, se hizo que el formulario de cada panel correspondiente se deshabilite si es para eliminar el registro

--- src/vista/VentanaInicio.java
+++ src/vista/VentanaInicio.java
@@ -774,6 +774,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[0][0].setEnabled(true);
 			interacciones[0][3].setVisible(true);
 			interacciones[0][3].setEnabled(true);
+			for(JTextField i:jtfsComprador) {	i.setEditable(true);};
 			if(src==menuItems[0][0]) {
 				interacciones[0][3].setVisible(false);
 				interacciones[0][3].setEnabled(false);
@@ -781,6 +782,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[0][0].setToolTipText("Agrega un nuevo comprador a la tabla");
 				lblOpComprador.setText("Altas");
 			}else if(src==menuItems[0][1]) {
+				metodoQueRestableceTODO(jtfsComprador);
+				for(JTextField i:jtfsComprador) {	i.setEditable(false);};
+				jtfsComprador[0].setEditable(true);
 				interacciones[0][0].setText("Eliminar");
 				interacciones[0][0].setToolTipText("Elimina el comprador de la tabla");
 				lblOpComprador.setText("Bajas");
@@ -801,6 +805,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[1][0].setEnabled(true);
 			interacciones[1][3].setVisible(true);
 			interacciones[1][3].setEnabled(true);
+			for(JTextField i:jtfsContratista) {	i.setEditable(true);};
 			if(src==menuItems[1][0]) {
 				interacciones[1][3].setVisible(false);
 				interacciones[1][3].setEnabled(false);
@@ -808,6 +813,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[1][0].setToolTipText("Agrega un nuevo contratista a la tabla");
 				lblOpContratista.setText("Altas");
 			}else if(src==menuItems[1][1]) {
+				metodoQueRestableceTODO(jtfsContratista);
+				for(JTextField i:jtfsContratista) {	i.setEditable(false);};
+				jtfsContratista[0].setEditable(true);
 				interacciones[1][0].setText("Eliminar");
 				interacciones[1][0].setToolTipText("Elimina el contratista de la tabla");
 				lblOpContratista.setText("Bajas");
@@ -828,6 +836,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[2][0].setEnabled(true);
 			interacciones[2][3].setVisible(true);
 			interacciones[2][3].setEnabled(true);
+			for(JTextField i:jtfsCriptomoneda) {	i.setEditable(true);};
 			if(src==menuItems[2][0]) {
 				interacciones[2][3].setVisible(false);
 				interacciones[2][3].setEnabled(false);
@@ -835,6 +844,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[2][0].setToolTipText("Agrega una nueva criptomoneda a la tabla");
 				lblOpCriptomoneda.setText("Altas");
 			}else if(src==menuItems[2][1]) {
+				metodoQueRestableceTODO(jtfsCriptomoneda);
+				for(JTextField i:jtfsCriptomoneda) {	i.setEditable(false);};
+				jtfsCriptomoneda[0].setEditable(true);
 				interacciones[2][0].setText("Eliminar");
 				interacciones[2][0].setToolTipText("Elimina la criptomoneda de la tabla");
 				lblOpCriptomoneda.setText("Bajas");
@@ -855,6 +867,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[3][0].setEnabled(true);
 			interacciones[3][3].setVisible(true);
 			interacciones[3][3].setEnabled(true);
+			for(JTextField i:jtfsPool) {	i.setEditable(true);};
 			if(src==menuItems[3][0]) {
 				interacciones[3][3].setVisible(false);
 				interacciones[3][3].setEnabled(false);
@@ -862,6 +875,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[3][0].setToolTipText("Agrega una nueva pool a la tabla");
 				lblOpPool.setText("Altas");
 			}else if(src==menuItems[3][1]) {
+				metodoQueRestableceTODO(jtfsPool);
+				for(JTextField i:jtfsPool) {	i.setEditable(false);};
+				jtfsPool[0].setEditable(true);
 				interacciones[3][0].setText("Eliminar");
 				interacciones[3][0].setToolTipText("Elimina la pool de la tabla");
 				lblOpPool.setText("Bajas");
@@ -882,6 +898,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[4][0].setEnabled(true);
 			interacciones[4][3].setVisible(true);
 			interacciones[4][3].setEnabled(true);
+			for(JTextField i:jtfsOrden) {	i.setEditable(true);};
 			if(src==menuItems[4][0]) {
 				interacciones[4][3].setVisible(false);
 				interacciones[4][3].setEnabled(false);
@@ -889,6 +906,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[4][0].setToolTipText("Agrega una nueva orden a la tabla");
 				lblOpOrden.setText("Altas");
 			}else if(src==menuItems[4][1]) {
+				metodoQueRestableceTODO(jtfsOrden);
+				for(JTextField i:jtfsOrden) {	i.setEditable(false);};
+				jtfsOrden[0].setEditable(true);
 				interacciones[4][0].setText("Eliminar");
 				interacciones[4][0].setToolTipText("Elimina la orden de la tabla");
 				lblOpOrden.setText("Bajas");
@@ -909,6 +929,7 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 			interacciones[5][0].setEnabled(true);
 			interacciones[5][3].setVisible(true);
 			interacciones[5][3].setEnabled(true);
+			for(JTextField i:jtfsOrdenDePotencia) {	i.setEditable(true);};
 			if(src==menuItems[5][0]) {
 				interacciones[5][3].setVisible(false);
 				interacciones[5][3].setEnabled(false);
@@ -916,6 +937,9 @@ class Interfaz extends JFrame implements ActionListener, ItemListener{
 				interacciones[5][0].setToolTipText("Agrega una nueva orden de potencia a la tabla");
 				lblOpOrdenDePotencia.setText("Altas");
 			}else if(src==menuItems[5][1]) {
+				metodoQueRestableceTODO(jtfsOrdenDePotencia);
+				for(JTextField i:jtfsOrdenDePotencia) {	i.setEditable(false);};
+				jtfsOrdenDePotencia[0].setEditable(true);
 				interacciones[5][0].setText("Eliminar");
 				interacciones[5][0].setToolTipText("Elimina la orden de potencia de la tabla");
 				lblOpOrdenDePotencia.setText("Bajas");
